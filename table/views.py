b836a8beb52723be4d5b9d400846bfe5dc92ccd1
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-02 20:11:50 +0545

    upload images

--- table/views.py
+++ table/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import ImageForm
 
 # Create your views here.
 def HomeView(request):
-    return render(request,'table/home.html')
+    if request.POST:
+     form=ImageForm(request.POST,request.FILES)
+
+     if form.is_valid():
+        form.save()
+    #  else:
+    #     print(form.errors)   
+    # else:
+    #    form=ImageForm()   
+    form=ImageForm()     
+
+    return render(request,'table/home.html',{
+        'form':form
+    })
